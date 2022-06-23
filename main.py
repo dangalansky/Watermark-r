25346d0c1db1e16b847dc6290b5728009f9ffd9b
dangalansky <dangalansky@gmail.com>
2022-06-23 12:03:00 -0600

    Update Code.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from tkinter import *
 from PIL import ImageTk, Image
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
 # ------------ GUI ------------- #
 window = Tk()
 window.title('Watermarking App!')
+window.config(padx=50, pady=50)
 
 # ------------Background------------- #
-canvas = Canvas(height=200, width=1000)
-canvas.grid(row=0, column=0)
-
+canvas = Canvas(height=200, width=50)
+canvas.grid(row=0, column=3)
 
 # ------------ Functions -------------#
 def open_watermark():
@@ -37,24 +37,33 @@ def create_watermark():
     watermark_entry.delete(0, END)
     photo_entry.delete(0, END)
     save_name_entry.delete(0, END)
+    messagebox.showinfo(title='Success!', message='Watermarked Image Has Been Created and Saved!')
 
 
-open_wtmrk = Button(window, text='Select Watermark', command=open_watermark).grid(row=1, column=1)
+
+watermark_label = Label(text='Watermark: ')
+watermark_label.grid(row=1, column=0)
 watermark = StringVar(None)
-watermark_entry = Entry(window, textvariable=watermark, width=50)
-watermark_entry.grid(row=1, column=0)
+watermark_entry = Entry(window, textvariable=watermark, width=30)
+watermark_entry.grid(row=1, column=1, pady=10)
+open_wtmrk = Button(window, text='Select File', command=open_watermark).grid(row=1, column=2)
 
 
-open_photo = Button(window, text='Select Photo', command=open_photo).grid(row=2, column=1)
+
+photo_label = Label(text='        Photo: ')
+photo_label.grid(row=2, column=0)
 photo = StringVar(None)
-photo_entry = Entry(window, textvariable=photo, width=50)
-photo_entry.grid(row=2, column=0)
+photo_entry = Entry(window, textvariable=photo, width=30)
+photo_entry.grid(row=2, column=1, pady=10)
+open_photo = Button(window, text='Select File', command=open_photo).grid(row=2, column=2)
 
 
-create_watermarked = Button(window, text='Save Photo', command=create_watermark).grid(row=3, column=1)
+save_label = Label(text='Save Photo As:        ')
+save_label.grid(row=3, column=0)
 save_name = StringVar(None)
-save_name_entry = Entry(window, textvariable=save_name, width=50)
-save_name_entry.grid(row=3, column=0)
+save_name_entry = Entry(window, textvariable=save_name, width=30)
+save_name_entry.grid(row=3, column=1, pady=10)
+create_watermarked = Button(window, text='Save', command=create_watermark).grid(row=3, column=2)
 
 
 
